fd7c4833b803e8b901dad033b8d3aa58fc1f21b5
omeruren <omer.uren19@gmail.com>
2026-06-23 18:19:17 +0300

    Added Validators for Create and Update Category

---
+++ src/Shoppy/Shoppy.Business/Categories/Validators/CategoryCreateValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Shoppy.Business.DataTransferObjects;
+
+namespace Shoppy.Business.Categories.Validators;
+
+public class CategoryCreateValidator : AbstractValidator<CategoryCreateDto>
+{
+    public CategoryCreateValidator()
+    {
+        RuleFor(c => c.Name)
+            .NotEmpty().WithMessage("Category name is required.")
+            .MaximumLength(100).WithMessage("Category name can not be more than 100 characters.");
+
+    }
+}
---
+++ src/Shoppy/Shoppy.Business/Categories/Validators/CategoryUpdateValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Shoppy.Business.DataTransferObjects;
+
+namespace Shoppy.Business.Categories.Validators;
+
+public class CategoryUpdateValidator : AbstractValidator<CategoryUpdateDto>
+{
+    public CategoryUpdateValidator()
+    {
+
+        RuleFor(c => c.Id).NotEmpty().WithMessage("Id is required.");
+
+        RuleFor(c => c.Name)
+            .NotEmpty().WithMessage("Category name is required.")
+            .MaximumLength(100).WithMessage("Category name can not be more than 100 characters.");
+
+    }
+}
