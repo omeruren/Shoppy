8b514b3d76d3d0e415f252d51147e04437ae86a8
omeruren <omer.uren19@gmail.com>
2026-06-23 18:06:28 +0300

    added custom ExceptionHandler file.

---
+++ src/Shoppy/Shoppy.WebAPI/Handlers/ExceptionHandler.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Shoppy.WebAPI.Filters;
+using System.Net;
+
+namespace Shoppy.WebAPI.Handlers;
+
+public class ExceptionHandler : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        if (exception is ValidationExceptionEx validationExceptionEx)
+        {
+            var errors = validationExceptionEx.Errors.SelectMany(e => e.Value).ToList();
+
+            httpContext.Response.StatusCode = (int)HttpStatusCode.UnprocessableContent;
+
+            await httpContext.Response.WriteAsJsonAsync(errors);
+
+            return true;
+        }
+
+        httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+        await httpContext.Response.WriteAsJsonAsync(new { exception.Message, httpContext.Response.StatusCode }, cancellationToken);
+
+        return true;
+    }
+}
--- src/Shoppy/Shoppy.WebAPI/Program.cs
+++ src/Shoppy/Shoppy.WebAPI/Program.cs
@@ -2,6 +2,7 @@ using Carter;
 using Scalar.AspNetCore;
 using Shoppy.Business;
 using Shoppy.DataAccess;
+using Shoppy.WebAPI.Handlers;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,9 +14,11 @@ builder.Services.AddDataAccess(builder.Configuration).AddBusiness();
 // Carter
 builder.Services.AddCarter();
 
-// Open Api
+// OPEN API
 builder.Services.AddOpenApi();
 
+// CUSTOM EXCEPTION HANDLER
+builder.Services.AddExceptionHandler<ExceptionHandler>().AddProblemDetails();
 
 var app = builder.Build();
 
