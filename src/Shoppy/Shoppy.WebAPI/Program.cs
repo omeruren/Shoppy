56b7c6ef0fd432f68277bd39aa439e1f69b47f3d
omeruren <omer.uren19@gmail.com>
2026-06-23 17:04:24 +0300

    registered Scalar and OpenApi
    installed Scalar and OpenApi packages.

--- src/Shoppy/Shoppy.WebAPI/Program.cs
+++ src/Shoppy/Shoppy.WebAPI/Program.cs
@@ -1,4 +1,5 @@
 using Carter;
+using Scalar.AspNetCore;
 using Shoppy.Business;
 using Shoppy.DataAccess;
 
@@ -12,8 +13,15 @@ builder.Services.AddDataAccess(builder.Configuration).AddBusiness();
 // Carter
 builder.Services.AddCarter();
 
+// Open Api
+builder.Services.AddOpenApi();
+
+
 var app = builder.Build();
 
+app.MapOpenApi();
+
+app.MapScalarApiReference();
 
 app.MapCarter();
 
