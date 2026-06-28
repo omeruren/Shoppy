c10deea88711300f60785910c1732fdb8ead9926
omeruren <omer.uren19@gmail.com>
2026-06-28 11:39:39 +0300

    Added User and Role Models.
    installed Microsoft.AspNetCore.Identity.EntityFrameworkCore package

---
+++ src/Shoppy/Shoppy.Entity/Models/Role.cs
@@ -0,0 +1,14 @@
+﻿using Shoppy.Entity.Abstraction;
+
+namespace Shoppy.Entity.Models;
+
+public sealed class Role : BaseEntity
+{
+    public string Name { get; set; } = default!;
+}
+
+public sealed class UserRole
+{
+    public Guid UserId { get; set; }
+    public Guid RoleId { get; set; }
+}
---
+++ src/Shoppy/Shoppy.Entity/Models/User.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Shoppy.Entity.Models;
+
+public sealed class User : IdentityUser<Guid>
+{
+    public string FirstName { get; set; } = default!;
+    public string LastName { get; set; } = default!;
+    public string FullName { get; set; } = default!;
+
+    public bool IsDeleted { get; set; }
+    public DateTimeOffset? DeletedAt { get; set; }
+
+
+    // CREATE USER
+    public static User Create(string firstName, string lastName, string userName, string email)
+    {
+        return new User
+        {
+            FirstName = firstName,
+            LastName = lastName,
+            UserName = userName,
+            Email = email,
+            FullName = $"{firstName} {lastName}"
+        };
+    }
+
+
+    // UPDATE USER
+    public void Update(string firstName, string lastName, string userName, string email)
+    {
+
+        FirstName = firstName;
+        LastName = lastName;
+        UserName = userName;
+        Email = email;
+        FullName = $"{firstName} {lastName}";
+    }
+
+
+    // DELETE USER
+    public void Delete()
+    {
+        IsDeleted = true;
+        DeletedAt = DateTimeOffset.UtcNow;
+    }
+}
